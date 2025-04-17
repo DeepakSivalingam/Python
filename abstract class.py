d57f23843f9a13e5ff4f05e2981812f2e3cd11e0
Deepak Sivalingam <160302202+DeepakSivalingam@users.noreply.github.com>
2025-04-17 13:12:31 +0530

    Update abstract class.py

--- abstract class.py
+++ abstract class.py
@@ -35,3 +35,19 @@ object1.start()
 '''
 
 
+# Encapsulation 
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance  # private variable
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = Account(1000)
+acc.deposit(500)
+print(acc.get_balance())  # ✅ 1500
+# print(acc.__balance) ❌ This will give an error (private)
+
