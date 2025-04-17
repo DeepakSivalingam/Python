e1ce2c36df7bf0cacd29c888ef0df42b2389de7a
Deepak Sivalingam <160302202+DeepakSivalingam@users.noreply.github.com>
2025-04-17 13:21:53 +0530

    Update math functions.py

--- math functions.py
+++ math functions.py
@@ -9,6 +9,10 @@ print(math.fabs(-9))
 print(math.pow(9,2))
 print(math.pi)
 print(math.e)
+print(math.fmod(10, 3)) 
+
+print(math.gcd(24, 36))      # Greatest common divisor → 12
+print(math.lcm(4, 6))        # Least common multiple → 12 (Python 3.9+)
 
 
 # another way for importing the math module and using 
